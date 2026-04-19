4331b2ce1bd68986c26d9704cdcd046b6ab7bf74
cesar.tomita <cesar.jun.tomita@gmail.com>
2026-04-19 14:35:39 -0300

    Add: The Pads

---
+++ challenges/the-pads/solution.sql
@@ -0,0 +1,19 @@
+SET NOCOUNT ON;
+
+SELECT
+    CONCAT(Name,'(',LEFT(Occupation,1),')')
+FROM
+    OCCUPATIONS
+ORDER BY
+    Name;
+
+SELECT
+    CONCAT('There are a total of ', COUNT(*),' ',LOWER(Occupation),'s.')
+FROM
+    OCCUPATIONS
+GROUP BY
+    Occupation
+ORDER BY
+    COUNT(*);
+
+go
